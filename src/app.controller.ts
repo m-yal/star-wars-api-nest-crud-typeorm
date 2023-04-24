dd592d40b14eb25174a3e804b2561f2f7db9effe
m-yal <maksym.yalovitsa@gmail.com>
2023-04-24 20:00:51 +0300

    dc healthcheck route fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiHeader, ApiTags } from "@nestjs/swagger";
+import { VERSION_HEADER, CURRENT_VERSION } from "./common/versioning/options";
 
-@Controller()
+@ApiTags("Hello world healthcheck path")
+@ApiHeader({ name: VERSION_HEADER, schema: { default: CURRENT_VERSION } })
+@Controller("/")
 export class AppController {
   @Get()
   helloWorld(): string {
