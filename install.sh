fa8adc874ddb5d3bb1ab982fa5648ee48ac64491
m-yal <maksym.yalovitsa@gmail.com>
2023-04-27 16:47:03 +0300

    dc certbot try 39

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
  
-# Phase 1
+echo "### Phase 1: generating initial certificate"
 mkdir -p certbot/data
-sudo chown -R $(whoami):$(whoami) etc
+sudo chown -R $(whoami):$(whoami) .
 docker compose -f ./docker-compose-initiate.yml up -d nginx
 docker compose -f ./docker-compose-initiate.yml up certbot
 docker compose -f ./docker-compose-initiate.yml down
- 
-# some configurations for let's encrypt
+sudo chown -R $(whoami):$(whoami) .
+
+echo "### Phase 2: setting some configurations for let's encrypt"
 sudo curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
 sudo openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
  
-# Phase 2
-chmod +x etc/crontab
+echo "### Phase 3: launching production docker compose"
 crontab ./etc/crontab
+sudo chown -R $(whoami):$(whoami) .
 docker compose -f ./docker-compose.yml up
