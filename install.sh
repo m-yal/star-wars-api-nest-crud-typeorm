882d652338e5b3556139f312510abc5fc458652b
m-yal <maksym.yalovitsa@gmail.com>
2023-04-27 16:02:15 +0300

    dc certbot try 34

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@
 # docker compose -f ./docker-compose-initiate.yaml down
  
 # some configurations for let's encrypt
-sudo curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
-sudo openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+# sudo curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+# sudo openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
  
 # Phase 2
 crontab ./etc/crontab
