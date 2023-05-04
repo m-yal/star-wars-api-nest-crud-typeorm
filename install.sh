30b9cf93b17d5e01fb8c78946fe458ff19a010bc
m-yal <maksym.yalovitsa@gmail.com>
2023-05-04 16:13:53 +0300

    tc 2

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 echo "### Phase 1: generating initial certificate"
 mkdir -p certbot/data
 mkdir images
+mkdir teamcity
+mkdir teamcity/agent-conf
+mkdir teamcity/datadir
+mkdir teamcity/logs
 sudo chown -R $(whoami):$(whoami) .
 docker compose -f ./docker-compose-initiate.yml up -d nginx
 docker compose -f ./docker-compose-initiate.yml up certbot
 docker compose -f ./docker-compose-initiate.yml down
-sudo chown -R $(whoami):$(whoami) .
 
 echo "### Phase 2: setting some configurations for let's encrypt"
 sudo curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
